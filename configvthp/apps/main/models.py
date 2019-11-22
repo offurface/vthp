7e651e43292edf2cf1f2965d30c635c6ccf61de8
RayCastHits <den.naz.den@gmail.com>
2019-11-23 00:52:43 +0300

    refactor code

--- configvthp/apps/main/models.py
+++ configvthp/apps/main/models.py
@@ -6,7 +6,6 @@ DAYS = (
     (3, "Среда"),
     (4, "Четверг"),
     (5, "Пятница"),
-
 )
 SEASON = (
     (1, "Лето"),
@@ -24,51 +23,64 @@ REASON_FOR_SKIPPING = (
     (1, "Некоторые инфекционные и паразитарные болезни"),
     (2, "Новообразования"),
     (3, "Болезни крови, кроветворных органов и отдельные нарушения, вовлекающие иммунный механизм"),
-    (4, ""),
-    (5, ""),
-    (6, ""),
-    (7, ""),
-    (8, ""),
-    (9, ""),
-    (10, ""),
-    (11, ""),
-    (12, ""),
-    (13, ""),
-    (14, ""),
-    (15, ""),
-    (16, ""),
-    (17, ""),
-    (18, ""),
-    (19, ""),
-    (20, ""),
-    (21, ""),
-    (22, ""),
-    (23, ""),
-    (24, ""),
-    (25, ""),
-    (26, ""),
-    (27, ""),
-    (28, ""),
+    (4, "Болезни эндокринной системы, расстройства питания и нарушения обмена веществ"),
+    (5, "Психические расстройства и расстройства поведения"),
+    (6, "Болезни нервной системы"),
+    (7, "Болезни глаза и его придаточного аппарата"),
+    (8, "Болезни уха и сосцевидного отростка"),
+    (9, "Болезни системы кровообращения"),
+    (10, "Болезни органов дыхания"),
+    (11, "Болезни органов пищеварения"),
+    (12, "Болезни кожи и подкожной клетчатки"),
+    (13, "Болезни костно-мышечной системы и соединительной ткани"),
+    (14, "Болезни мочеполовой системы"),
+    (15, "Беременность, роды и послеродовой период"),
+    (16, "Отдельные состояния, возникающие в перинатальном периоде"),
+    (17, "Врожденные аномалии [пороки развития], деформации и хромосомные нарушения"),
+    (18, "Симптомы, признаки и отклонения от нормы, выявленные при клинических и лабораторных исследованиях, не классифицированные в других рубриках"),
+    (19, "Травмы, отравления и некоторые другие последствия воздействия внешних причин"),
+    (20, "Внешние причины заболеваемости и смертности"),
+    (21, "Факторы, влияющие на состояние здоровья населения и обращения в учреждения здравоохранения"),
+    (22, "Осмотр после лечения"),
+    (23, "Медицинская консультаци"),
+    (24, "Донорство крови"),
+    (25, "Сдача анализов"),
+    (26, "Отсутствие без объяснения причины"),
+    (27, "Психотерапия "),
+    (28, "Посещение стоматолога"),
+)
+PASS_MONTH = (
+    (1, "Январь"),
+    (2, "Февраль"),
+    (3, "Март"),
+    (4, "Апрель"),
+    (5, "Май"),
+    (6, "Июнь"),
+    (7, "Июль"),
+    (8, "Август"),
+    (9, "Сентябрь"),
+    (10, "Октябрь"),
+    (11, "Ноябрь"),
+    (12, "Декабрь"),
 )
 
-
 class Student(models.Model):
-    dynamometry_left = models.PositiveIntegerField(choices=REASON_FOR_SKIPPING, blank=True, null=True,
+    reason_for_skipping = models.PositiveIntegerField(choices=REASON_FOR_SKIPPING, blank=True, null=True,
     verbose_name="Причина пропуска")
-    dynamometry_left = models.PositiveIntegerField(choices=REASON_FOR_SKIPPING, blank=True, null=True,
+    pass_month = models.PositiveIntegerField(choices=PASS_MONTH, blank=True, null=True,
     verbose_name="Месяц пропуска")
-    dynamometry_left = models.PositiveIntegerField(choices=DAYS, blank=True, null=True,
+    day_of_the_week = models.PositiveIntegerField(choices=DAYS, blank=True, null=True,
     verbose_name="День недели")
-    dynamometry_left = models.PositiveIntegerField(choices=SEASON, blank=True, null=True,
+    season = models.PositiveIntegerField(choices=SEASON, blank=True, null=True,
     verbose_name="Время года")
 
-    dynamometry_left = models.PositiveIntegerField(blank=True, null=True,
+    transportation_costs = models.PositiveIntegerField(blank=True, null=True,
     verbose_name="Траты на транспорт")
-    dynamometry_left = models.PositiveIntegerField(blank=True, null=True,
+    distance = models.PositiveIntegerField(blank=True, null=True,
     verbose_name="Расстояние от дома до места обучения")
-    dynamometry_left = models.PositiveIntegerField(blank=True, null=True,
+    age = models.PositiveIntegerField(blank=True, null=True,
     verbose_name="Возраст")
-    dynamometry_left = models.BooleanField(
+    disciplinary_action = models.BooleanField(
     verbose_name="Дисциплинарное взыскание")
 
     def __str__(self):
